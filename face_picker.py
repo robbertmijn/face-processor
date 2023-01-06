91fe23c7f7f2d23adb4fbb1891ed6676197cdcaf
robbertmijn <robbertmijn@gmail.com>
2023-01-06 14:36:03 +0100

    update

--- face_picker.py
+++ face_picker.py
@@ -5,12 +5,14 @@ Created on Wed Dec  7 17:20:06 2022
 
 @author: robbertmijn
 """
-# import face_processor
+
 from face_processor import face_processor as fp
-# import glob
 import os
 from datamatrix import io, operations as ops
+import glob
+
 #%% Face selection
+
 """
 From the 2.222 faces we make a subset:
     - exclude facial hair
@@ -20,6 +22,8 @@ From the 2.222 faces we make a subset:
     - exclude really old and really young
 
 This should leave 915 faces
+
+Filenames and attributes of these faces are stored in a csv file. The actual files are stored in a separate directory.
 """
 
 # load demographics
@@ -31,26 +35,42 @@ dm["Gender"] = ["m" if x == 1 else "f" for x in dm["Gender"] ]
 # get subset based on attributes from csv
 subset = (dm["Facial hair?"] == 0) & (dm["Face direction?"] == 1) & (dm["Image quality"] > 2) & (dm["Age"] < 4) & (dm["Age"] > 2) & (dm["Emotion?"] == {0, 1})
 
-#%%
-orig_dir = "../10k US Adult Faces Database/Face Images"
+#%% Process the faces from the subset we just created and store them
 
-outdir = "original"
-if not os.path.exists(outdir):
-    os.makedirs(outdir)
+faces_dir = "../10k US Adult Faces Database/Face Images" # This is where I have them stored
+outdir_faces = "faces"
+
+# create directories if needed
+if not os.path.exists(outdir_faces):
+    os.makedirs(outdir_faces)
 else:
     pass
 
+# go over the list of subset images and create new images (split for gender, which we need for our experiment)
 for g, gdm in ops.split(subset.Gender):
     i = 0
-    for row in gdm:
-        
-        path = os.path.join(orig_dir, row.Filename)
+    for row in gdm:      
+        path = os.path.join(faces_dir, row.Filename)
         outname = "{}_{}".format(g, i)
         print("Loading {}, ({})".format(path, i))
-        img = fp.process_face(path, outdir, outname)
+        img = fp.process_face(path, outdir_faces, outname)
         if img is not None:
             i += 1
+            
+#%% Process faces that are stored separately in a directory (e.g., probes)
 
+probes_dir = "probes_raw"
+outdir_probes = "probes"
 
+if not os.path.exists(outdir_probes):
+    os.makedirs(outdir_probes)
+else:
+    pass
 
-
+i = 0
+for path in glob.glob(os.path.join(probes_dir, "*.jpg")):
+    outname = "probe_{}".format(i)
+    print("Loading {}, ({})".format(path, i))
+    img = fp.process_face(path, outdir_probes, outname)
+    if img is not None:
+        i += 1
--- face_processor/face_processor.py
+++ face_processor/face_processor.py
@@ -18,122 +18,86 @@ import numpy as np
 import cv2
 import dlib
 import os
+from matplotlib import pyplot as plt
 
 # load the face detector and shape predictor
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(os.path.join("face_processor", "shape_predictor_68_face_landmarks.dat"))
 
-def process_face(inpath, outdir, outname):
+# mean H/W ratio is 1.27
+W = 400
+H = int(W * 1.27)
+CROP = .75
     
-    # Get the image from file
+def process_face(inpath, outdir, outname, debug = False):
+    
+    # Get the image from file and force to size
     img = cv2.imread(inpath)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+        
+    new_img = _img_to_size(img, W, H)
     
     # detect faces
-    dets = detector(img, 1)
-    
+    dets = detector(new_img, 1)   
     if len(dets) != 1:
         # if 0 or more than 1 faces are detected we stop
         return None
     
     # extract landmarks from image
-    landmarks = shape_to_np(predictor(img, dets[0]))
+    landmarks = _shape_to_np(predictor(new_img, dets[0]))    
+    left_eye, right_eye, nose = _get_tri_points(landmarks)
     
-    # Extract center-points of both eyes and the nose
-    left_eye = [landmarks[42:47, 0].mean(), landmarks[42:47, 1].mean()]
-    right_eye = [landmarks[36:41, 0].mean(), landmarks[36:41, 1].mean()]
-    nose = [landmarks[27:35, 0].mean(), landmarks[27:35, 1].mean()]
-    face_rect = dets[0]
-    
-    # # add landmarks
-    # for lm in landmarks:        
-    #     img = cv2.circle(img, lm, 2, 255, -1)
-    #     img = cv2.rectangle(img, (dets[0].left(), dets[0].top()), 
-    #                         (dets[0].right(), dets[0].bottom()), 255)
-    
-    # Process the face image
-    img = transform_image(img, right_eye, left_eye, nose, face_rect)
-    
-    if img is not None:
+    # Transform the image (shift, rotate, mask)
+    new_img = _transform_image(new_img, right_eye, left_eye, nose)
+
+    # Save new image
+    if new_img is not None:
+        
         outpath = os.path.join(outdir, "{}.jpg".format(outname))
-        imsaved = cv2.imwrite(outpath, img)
+        imsaved = cv2.imwrite(outpath, new_img)
+
         if imsaved:
             print("saving {}".format(outpath))
         else:
             print("saving error")
+            
     else:
-        print("can't transform")
-        
-    return img
+        print("can't transform")       
 
-def transform_image(img, right_eye, left_eye, nose, face_rect):
+    return new_img
     
-    # Hard coded image dimensions
-    W = 300
-    H = 350
     
-    # ignore too large images
-    if img.shape[0] > H:
-        return None
-        
+def _transform_image(im, right_eye, left_eye, nose):
+    
     # Determine where to shift the image to based on the area between the eyes and nose
-    x_shift = int(img.shape[1]/2 - np.array([right_eye[0], left_eye[0], nose[0]]).mean())
-    y_shift = int(img.shape[0]/2 - np.array([right_eye[1], left_eye[1], nose[1]]).mean())
+    x_shift = int(im.shape[1]/2 - np.array([right_eye[0], left_eye[0], nose[0]]).mean())
+    y_shift = int(im.shape[0]/2 - np.array([right_eye[1], left_eye[1], nose[1]]).mean())
     
     # Determine the rotation to get the eyes on the same level horizontally
     rotation = np.degrees(np.arctan((right_eye[1] - left_eye[1]) / (right_eye[0] - left_eye[0])))
     
-    # TODO size transformation
+    # TODO scale factor based on facial features
+    # x, y = zip(left_eye, right_eye, nose)
+    # scale = FACE_AREA / get_area(x, y)
     
-    # Determine how much padding is required to get the desired size of the image
-    pad_x = int((W - img.shape[1])/2)
-    pad_y = int((H - img.shape[0])/2)
+    im = _do_transform(im, x_shift, y_shift, rotation)
     
-    # Specify the transformation (The mask must be transformed too!)
-    def do_transform(im):
-        # Pad and add extra room to accomodate the rotation
-        im = cv2.copyMakeBorder(im, 100 + pad_y, 100 + pad_y, 100 + pad_x, 100 + pad_x, cv2.BORDER_CONSTANT, value = 0)
-        im = imutils.rotate(im, angle = rotation)
-        im = imutils.translate(im, x_shift, y_shift)
-        # Remove the extra room
-        im = im[100:(H+100), 100:(W+100)]
-        return im
+    im, mask = _mask_image(im)
     
-    new_img = do_transform(img)
-    
-    # Mask the image
-    # TODO mask is now hard coded to be 90 by 70, make this variable
-    mask = np.zeros_like(new_img)
-    mask = cv2.ellipse(mask, 
-                        (int(W/2), int(H/2)), 
-                        (70, 100),
-            0, 0, 360, (255, 255, 255), -1)
-    new_img = np.bitwise_and(new_img, mask)
-     
-    # Reduce contrast by "50"
-    # TODO use equalizeHist or CLAHE to equalize contrast
-    contrast = -50
-    new_img[mask > 127] = new_img[mask > 127] * (contrast/127+1) - contrast
-    new_img[mask > 127] = np.clip(new_img[mask > 127], 0, 255)
-    
-    # On average, each pixel should be 127 (gray: our luminosity aim)
-    lum_aim = np.sum(mask) * 127
-    new_img[mask > 127] = new_img[mask > 127] + (lum_aim - new_img[mask > 127].sum()) / np.sum([mask > 127])
-    
-    # # Output mean luminance and standard deviation (metric for how much contrast there is)
-    # print("after, std {}, mean {}".format(new_img[mask > 127].std(), new_img[mask > 127].mean()))
-    
+    im = _set_contrast_and_luminance(im, mask)       
+
     # Turn the background gray (127)
-    new_img[mask < 127] = 127
+    im[mask < 127] = 127
     
     # if we had to shift on the horizontal axis too much, we discard
     if abs(x_shift) > 32:
         print("off center by {}".format(x_shift))
         return None
     
-    return new_img
+    return im
 
-def shape_to_np(shape, dtype="int"):
+
+def _shape_to_np(shape, dtype = "int"):
     
 	# initialize the list of (x, y)-coordinates
 	coords = np.zeros((68, 2), dtype=dtype)
@@ -145,3 +109,94 @@ def shape_to_np(shape, dtype="int"):
         
 	# return the list of (x, y)-coordinates
 	return coords    
+
+
+def _get_area(x, y):
+        return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+
+
+def _get_tri_points(landmarks):
+    
+    # Extract center-points of both eyes and the nose
+    left_eye = [landmarks[42:47, 0].mean(), landmarks[42:47, 1].mean()]
+    right_eye = [landmarks[36:41, 0].mean(), landmarks[36:41, 1].mean()]
+    nose = [landmarks[27:35, 0].mean(), landmarks[27:35, 1].mean()]
+    
+    return left_eye, right_eye, nose
+    
+def _img_to_size(im, W, H):
+    
+    # scale to height
+    scale = H / im.shape[0]  
+    
+    dims = (int(im.shape[1] * scale), H)
+    
+    im = cv2.resize(im, dims, interpolation = cv2.INTER_AREA)
+    im = cv2.copyMakeBorder(im, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value = 0)
+    
+    strt_idx_W = (im.shape[1] // 2) - (W // 2)
+    end_idx_W = (im.shape[1] // 2) + (W // 2)
+
+    strt_idx_H = (im.shape[0] // 2) - (H // 2)
+    end_idx_H = (im.shape[0] // 2) + (H // 2)
+    
+    im = im[strt_idx_H:end_idx_H, strt_idx_W:end_idx_W]
+    
+    return im
+
+
+def _mask_image(im):
+    
+    # Mask the image
+    mask = np.zeros_like(im)
+    mask = cv2.ellipse(mask, 
+                        (int(W/2), int(H/2)), 
+                        (int(W/2 * CROP), int(H/2 * CROP)),
+            0, 0, 360, (255, 255, 255), -1)
+    im = np.bitwise_and(im, mask)
+     
+    return im, mask
+   
+
+def _set_contrast_and_luminance(im, mask):
+    
+    # Reduce contrast by "50"
+    # TODO use equalizeHist or CLAHE to equalize contrast
+    contrast = -50
+    im[mask > 127] = im[mask > 127] * (contrast/127+1) - contrast
+    im[mask > 127] = np.clip(im[mask > 127], 0, 255)
+    
+    # On average, each pixel should be 127 (gray: our luminosity aim)
+    lum_aim = np.sum(mask) * 127
+    im[mask > 127] = im[mask > 127] + (lum_aim - im[mask > 127].sum()) / np.sum([mask > 127])
+
+    return im
+
+def _do_transform(im, x_shift, y_shift, rotation):
+    
+    # Pad and add extra room to accomodate the rotation
+    im = cv2.copyMakeBorder(im, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value = 0)
+    
+    im = imutils.rotate(im, angle = rotation)
+    im = imutils.translate(im, x_shift, y_shift)
+    
+    im = im[100:-100, 100:-100]
+    
+    return im
+    
+    
+def plt_landmarks(img, landmarks, rect):
+   
+   left_eye, right_eye, nose = _get_tri_points(landmarks) 
+   
+    # add landmarks
+   for lm in landmarks:        
+        img = cv2.circle(img, lm, 2, 255, -1)
+
+   img = cv2.rectangle(img, (rect.left(), rect.top()), 
+                       (rect.right(), rect.bottom()), 255)
+    
+   pts = np.array([left_eye, right_eye, nose], np.int32)
+   img = cv2.polylines(img, [pts], True, 255, 2)
+    
+   plt.imshow(img)
